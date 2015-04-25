e02ef516d847627b3d7c1bbdb52e3675be98ba8c
Vitaliy Myronenko <vmyronenko@gmail.com>
2015-04-25 10:53:58 -0500

    Added TeamMember controller

---
+++ src/TeamTasker.Web/TeamTasker.Web/Domain/TaskerContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TeamTasker.Web.Domain
+{
+    public class TaskerContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<TeamMember> TeamMembers { get; set; }
+    }
+}
---
+++ src/TeamTasker.Web/TeamTasker.Web/Domain/TeamMember.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TeamTasker.Web.Domain
+{
+    public class TeamMember
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public List<Task> AssignedTasks { get; set; }
+    }
+}
