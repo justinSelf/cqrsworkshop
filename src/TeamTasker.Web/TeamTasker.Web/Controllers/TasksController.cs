c809fdd811504e5388939eb5f261d9051a875d64
Vitaliy Myronenko <vmyronenko@gmail.com>
2015-04-25 13:45:15 -0500

    Added Task controller

---
+++ src/TeamTasker.Web/TeamTasker.Web/Controllers/TasksController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeamTasker.Web.Domain;
+
+namespace TeamTasker.Web.Controllers
+{
+    public class TasksController : Controller
+    {
+        private TaskerContext db = new TaskerContext();
+
+        [HttpPost]
+        public JsonResult Create(string name, DateTime dueDate, string instructions)
+        {
+            var task = new Task
+            {
+                Name = name,
+                DueDate = dueDate,
+                Instructions = instructions
+            };
+
+            db.Tasks.Add(task);
+            db.SaveChanges();
+
+            return Json("success");
+        }
+    }
+}
