17aa864cfbb95a4369bb8ab5940c339f7e53b6c1
Vitaliy Myronenko <vmyronenko@gmail.com>
2015-04-25 11:45:50 -0500

    Fixed db context

--- src/TeamTasker.Web/TeamTasker.Web/Controllers/TeamMemberController.cs
+++ src/TeamTasker.Web/TeamTasker.Web/Controllers/TeamMemberController.cs
@@ -9,10 +9,16 @@ namespace TeamTasker.Web.Controllers
 {
     public class TeamMemberController : Controller
     {
+        private TaskerContext db = new TaskerContext();
 
         [HttpPost]
         public JsonResult Add(string name)
         {
+            var teamMember = new TeamMember { Name = name };
+
+            db.TeamMembers.Add(teamMember);
+            db.SaveChanges();
+
             return Json("success");
         }
 
